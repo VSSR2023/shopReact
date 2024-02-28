b6e0388d44d26e4c977561ff033a6463feea8b22
VSSR2023 <135501916+VSSR2023@users.noreply.github.com>
2024-02-28 20:54:55 +0400

    clear_project_directory_add_comp
    Clear project in public and src and add directory components with header with added components and footer

---
+++ src/components/Header.jsx
@@ -0,0 +1,15 @@
+import React from "react"
+
+export default function Header(){
+    return(
+        <header>
+            <div>
+                <span className="logo">Чудо-Магазин</span>
+            </div>
+            <div className=>
+
+            </div>
+
+        </header>
+    );
+}
